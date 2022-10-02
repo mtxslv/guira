6ae7a9ccce062c3e54159588ff72f1b06a9c760f
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-10-02 14:48:58 -0300

    docstring changed

--- guira/guira_exceptions.py
+++ guira/guira_exceptions.py
@@ -2,7 +2,7 @@ from lib2to3.pytree import Base
 
 
 class SimulatorException(Exception): 
-    """Simulator Exceptions
+    """Exceptions related to the Simulator.
 
     Args:
         Exception (class): base class for exception
