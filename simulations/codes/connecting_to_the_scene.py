f39f4ddea1d6b39b326c99a295bcc7ef4f49d428
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-10-04 21:52:38 -0300

    comments fixed

--- simulations/codes/connecting_to_the_scene.py
+++ simulations/codes/connecting_to_the_scene.py
@@ -1,4 +1,6 @@
 # To run this tutorial do:
+# Open the Scene connecting_to_the_scene.ttt
+# Or...
 # 1. Open a new scene in CoppeliaSim Edu V4.3.0
 # 2. At the end of the scene's main script, add: simRemoteApi.start(19999)
 # 3. Run the simulation
