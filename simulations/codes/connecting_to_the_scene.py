57a1d07a7e2fe3e59a0cf806a6baa4fa919e98aa
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-10-04 21:51:42 -0300

    first tutorial

---
+++ simulations/codes/connecting_to_the_scene.py
@@ -0,0 +1,15 @@
+# To run this tutorial do:
+# 1. Open a new scene in CoppeliaSim Edu V4.3.0
+# 2. At the end of the scene's main script, add: simRemoteApi.start(19999)
+# 3. Run the simulation
+
+from guira.scene import Scene
+
+def main():
+    scene = Scene()
+    scene.connect()
+    how_many_objs = scene.test_connection()
+    print(f'There are {how_many_objs} objects in the scene') # 15 objects
+
+if __name__ == "__main__":
+    main()
