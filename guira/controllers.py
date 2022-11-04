f2772a8f5fb5a006ea2b0a87e5113f9ea32f4d72
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-11-03 21:40:35 -0300

    init first definitions

--- guira/controllers.py
+++ guira/controllers.py
@@ -1,6 +1,29 @@
 class PositionController():
-    def __init__(self):
-        pass
+    """The Position Controller decouple input signals in order 
+       to explicit the angular and linear errors. The position
+       is controlled by minimizing such errors.
+    """
+    def __init__(self, 
+                 linear_coef: float = 0.3,
+                 angular_coef: float = 0.2,
+                 left_wheel_radius: float = 0.0423005,
+                 right_wheel_radius: float = 0.0423005,
+                 distance_between_wheels: float = 0.214):
+        """
+
+        Args:
+            linear_coef (float, optional): _description_. Defaults to 0.3.
+            angular_coef (float, optional): _description_. Defaults to 0.2.
+            left_wheel_radius (float, optional): The radius of the robot's left wheel. Defaults to 0.0423005.
+            right_wheel_radius (float, optional): The radius of the robot's right wheel. Defaults to 0.0423005.
+            distance_between_wheels (float, optional): Distance between the wheels. Defaults to 0.214.
+        """
+        self.linear_coef = linear_coef
+        self.angular_coef = angular_coef
+        self.left_wheel_radius = left_wheel_radius
+        self.right_wheel_radius = right_wheel_radius
+        self.distance_between_wheels = distance_between_wheels
+
     def set_positions(self, positions: list):
         pass
     def control(self):
